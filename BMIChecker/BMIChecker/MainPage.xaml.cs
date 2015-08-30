128e4c65c5374b2b78a507846508c7f38691f7bc
Tomokazu Kizawa <tkizawa@msn.com>
2015-08-30 15:21:44 +0900

    機能実装版

--- BMIChecker/BMIChecker/MainPage.xaml.cs
+++ BMIChecker/BMIChecker/MainPage.xaml.cs
@@ -22,9 +22,129 @@ namespace BMIChecker
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private bmicalc bmi = new bmicalc();
+
         public MainPage()
         {
             this.InitializeComponent();
+            ClearTextBox();
+            txtHeight.Text = "174";
+            txtWeight.Text = "63.6";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextBox();
+        }
+
+        private void ClearTextBox()
+        {
+            txtResultBMI.Text = "";
+            txtResulStdWeight.Text = "";
+            txtResultDiffStdWeight.Text = "";
+            txtResult.Text = "";
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            string strMessage = string.Empty;
+            while (true)
+            {
+                // テキストボックスの確認をする。
+                // 身長の確認
+                if (txtHeight.Text == "")
+                {
+                    strMessage = "身長を入力してください。";
+                    break;
+                }
+
+                // 体重の確認
+                if (txtWeight.Text == "")
+                {
+                    strMessage = "体重を入力してください。";
+                    break;
+                }
+
+                // パラメータチェックする
+                // 型変換する
+                //float fHeight=(float)(txtHeight.Text)
+                float fHeight = float.Parse(txtHeight.Text)/100;
+                float fWeight = float.Parse(txtWeight.Text);
+                var result = bmi.CheckParam(fHeight, fWeight);
+                switch (result)
+                {
+                    case ErrorCode.NoERR:
+                        break;
+                    case ErrorCode.Height:
+                        strMessage = "身長が0cmになっています。正しい値を入れて下さい。";
+                        // TODO:メッセージを出力する
+                        break;
+                    case ErrorCode.Weight:
+                        strMessage = "体重が0kgになっています。正しい値を入れて下さい。";
+                        // TODO:メッセージを出力する
+                        break;
+                    default:
+                        break;
+                }
+                if (result != ErrorCode.NoERR)
+                    break;
+
+                // BMIの計算をする
+                float fBMI = fWeight / (fHeight * fHeight);
+                if (fBMI == 0)
+                {
+                    strMessage = "BMIが正常に算出できませんでした。";
+                    break;
+                }
+                txtResultBMI.Text = fBMI.ToString("#.#");
+
+                // 標準体重
+                float fStdWeight = (fHeight * fHeight * 22);
+                txtResulStdWeight.Text = fStdWeight.ToString("#.#");
+
+                // 標準体重との差
+                float fDiff = fWeight - fStdWeight;
+                txtResultDiffStdWeight.Text = fDiff.ToString("#.#");
+
+                // 判定
+                string strResult = string.Empty;
+                // BMIより判定
+                if (fBMI < 18.5)
+                {
+                    // 痩せています
+                    strResult = "痩せています";
+                }
+                if ((fBMI >= 18.5) && (fBMI < 25))
+                {
+                    // 標準体重
+                    strResult = "標準体重";
+                }
+                if ((fBMI >= 25) && (fBMI < 30))
+                {
+                    // 肥満レベル1
+                    strResult = "肥満レベル1";
+                }
+                if ((fBMI >= 30) && (fBMI < 35))
+                {
+                    // 肥満レベル2
+                    strResult = "肥満レベル2";
+                }
+                if ((fBMI >= 35) && (fBMI < 40))
+                {
+                    // 肥満レベル3
+                    strResult = "肥満レベル3";
+                }
+                if (fBMI >= 40)
+                {
+                    // 肥満レベル4
+                    strResult = "肥満レベル4";
+                }
+
+                txtResult.Text = strResult;
+
+                break;
+            }
         }
     }
 }
---
+++ BMIChecker/BMIChecker/bmicalc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIChecker
+{
+    /// <summary>
+    /// BMI計算のクラス
+    /// </summary>
+    class bmicalc
+    {
+
+        /// <summary>
+        /// パラメータチェック
+        /// </summary>
+        /// <param name="Height">身長</param>
+        /// <param name="Weight">体重</param>
+        /// <returns></returns>
+        public ErrorCode CheckParam(float Height, float Weight)
+        {
+            ErrorCode retErr = ErrorCode.NoERR;
+
+            if (Height == 0)
+            {
+                retErr = ErrorCode.Height;
+            }
+            if (Weight == 0)
+            {
+                retErr = ErrorCode.Weight;
+            }
+
+            return (retErr);
+        }
+    }
+
+    /// <summary>
+    /// エラーコード
+    /// </summary>
+    enum ErrorCode
+    {
+        NoERR,
+        Height,
+        Weight,
+        Calc,
+    }
+}
