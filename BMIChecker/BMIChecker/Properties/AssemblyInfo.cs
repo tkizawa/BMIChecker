5be68e49ff8a8973aca1148f09f6b540972dda29
Tomokazu Kizawa <tkizawa@msn.com>
2016-03-16 00:25:17 +0900

    パッケージ化作業中

--- BMIChecker/BMIChecker/Properties/AssemblyInfo.cs
+++ BMIChecker/BMIChecker/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BMIChecker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BMI肥満度チェッカー")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BMIChecker")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Tomokazu Kizawa Allrights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
